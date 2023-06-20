c79addd3713d894837e19bb3ccbb86261d50b5f4
IdanPipano <102733803+IdanPipano@users.noreply.github.com>
2023-06-20 11:48:23 +0300

    Changed location to Technion

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -46,6 +46,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//
+        LatLng technion = new LatLng(32, 35);
+        mMap.addMarker(new MarkerOptions().position(technion).title("Marker in technion"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(technion));
     }
 }
